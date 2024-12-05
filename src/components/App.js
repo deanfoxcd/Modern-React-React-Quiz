7eaf6b0e1b8282d54bbcce4d878074e221014405
Dean Fox <deanfoxcd@gmail.com>
2024-12-04 20:52:33 -0500

    add next question functionality

--- src/components/App.js
+++ src/components/App.js
@@ -6,11 +6,14 @@ import Loader from './Loader';
 import Error from './Error';
 import StartScreen from './StartScreen';
 import Question from './Question';
+import NextButton from './NextButton';
 
 const initialState = {
   questions: [],
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -21,13 +24,27 @@ function reducer(state, action) {
       return { ...state, status: 'error' };
     case 'start':
       return { ...state, status: 'active' };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      const correctAnswer = question.correctOption;
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === correctAnswer
+            ? state.points + question.points
+            : state.points,
+      };
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error('Action unknown');
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -56,7 +73,16 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions[index]} />}
+        {status === 'active' && (
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,9 +1,22 @@
-export default function Options({ question }) {
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
   return (
     <>
       <div className="options">
-        {question.options.map((o) => (
-          <button className="btn btn-option" key={o}>
+        {question.options.map((o, i) => (
+          <button
+            className={`btn btn-option ${i === answer ? 'answer' : ''} ${
+              hasAnswered
+                ? i === question.correctOption
+                  ? 'correct'
+                  : 'wrong'
+                : ''
+            }`}
+            key={o}
+            disabled={hasAnswered}
+            onClick={() => dispatch({ type: 'newAnswer', payload: i })}
+          >
             {o}
           </button>
         ))}
